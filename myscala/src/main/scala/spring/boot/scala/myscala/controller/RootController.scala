25b2537630823c1b3de2772b78839b81fd55483a
jmhbox2017 <jmhbox@yeah.net>
2019-09-30 14:08:16 +0800

    add myscala module

---
+++ myscala/src/main/scala/spring/boot/scala/myscala/controller/RootController.scala
@@ -0,0 +1,23 @@
+package spring.boot.scala.myscala.controller
+
+import java.time.LocalDateTime
+
+import io.micrometer.core.annotation.Timed
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.RequestMethod.GET
+import org.springframework.web.bind.annotation.{RequestMapping, RestController}
+
+@RestController
+class RootController {
+  @Value("${application.name}")
+  val appName: String = null
+
+  @RequestMapping(path = Array("/"), method = Array(GET))
+  @Timed
+  def root(): Map[String, Any] = {
+    Map("name" -> appName,
+      "java.version" -> System.getProperty("java.version"),
+      "now" -> LocalDateTime.now())
+  }
+
+}
